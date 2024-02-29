c51d2ec77b6567ede3bdd75e29e87afc1e4fa383
Ankit Gupta <ag_3112@outlook.com>
2024-02-29 11:00:32 +0530

    moved to the new package

--- tests/processors/test_data_processor.py
+++ tests/processors/test_data_processor.py
@@ -6,12 +6,7 @@ from processors.data_processor import process_data
 def test_process_data(spark_fixture):
     result = process_data(spark_fixture, 'tests/data/', '')
     assert 'col' in result.columns
-
-
-def test_process_data_1(spark_fixture):
-    with pytest.raises(Exception) as exec_info:
-        process_data(spark_fixture, 'tests/dat/', '')
-    assert str(exec_info.value) == 'Something wrong !'
+    assert result.count() == 13
 
 
 def test_process_data_1(spark_fixture):
